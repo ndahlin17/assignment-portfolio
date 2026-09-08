477ee78e5ec521fc93a206ed91bb79d8932f2073
ndahlin17 <nickyd1787@gmail.com>
2026-09-08 10:36:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GitHub Pages Template for Hello World
+This repository provides a template for a GitHub Pages website. The template implements HTML, CSS, and JavaScript. 
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Starting Hello, World.")
+
+function makeGreen() {
+	console.log("Calling makeGreen function")
+	document.getElementById("msg").classList.add("green");
+}
